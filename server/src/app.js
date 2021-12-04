b619e31daae8921d9063a57eedf36b7ca1a20389
KiyotoRyuman <appleumasugi.mjd@gmail.com>
2021-12-04 20:33:42 +0900

    feat: Gzip化

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,17 @@
 import bodyParser from 'body-parser';
 import Express from 'express';
 import session from 'express-session';
+import compression from 'compression';
 
 import { apiRouter } from './routes/api';
 import { staticRouter } from './routes/static';
 
 const app = Express();
+
+app.use(compression({level: 6}));
+
+var maxTime = 86400000 * 30;
+app.use(Express.static(__dirname + "/public", {maxAge: maxTime}))
 
 app.set('trust proxy', true);
 
