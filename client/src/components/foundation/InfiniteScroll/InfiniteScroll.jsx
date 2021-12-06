3cc007ddba60dcd7fed14623359d98e737d38248
KiyotoRyuman <appleumasugi.mjd@gmail.com>
2021-12-07 00:34:54 +0900

    revert infinite

--- client/src/components/foundation/InfiniteScroll/InfiniteScroll.jsx
+++ client/src/components/foundation/InfiniteScroll/InfiniteScroll.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useInView } from 'react-intersection-observer';
 
 /**
  * @typedef {object} Props
@@ -10,27 +9,45 @@ import { useInView } from 'react-intersection-observer';
 
 /** @type {React.VFC<Props>} */
 const InfiniteScroll = ({ children, fetchMore, items }) => {
-  const { ref, inView } = useInView();
   const latestItem = items[items.length - 1];
+
   const prevReachedRef = React.useRef(false);
 
   React.useEffect(() => {
-    if (!inView || latestItem === prevReachedRef.current) {
-      return;
-    }
-    // アイテムがないときは追加で読み込まない
-    if (latestItem !== undefined) {
-      fetchMore();
-    }
-    prevReachedRef.current = latestItem;
-  }, [latestItem, inView]);
-  
-  return (
-    <>
-      {children}
-      <div ref={ref} style={{ height: 1, width: '100%' }} />
-    </>
-  );
+    const handler = () => {
+      // 念の為 2の18乗 回、最下部かどうかを確認する
+      const hasReached = Array.from(Array(2 ** 18), () => {
+        return window.innerHeight + Math.ceil(window.scrollY) >= document.body.offsetHeight;
+      }).every(Boolean);
+
+      // 画面最下部にスクロールしたタイミングで、登録したハンドラを呼び出す
+      if (hasReached && !prevReachedRef.current) {
+        // アイテムがないときは追加で読み込まない
+        if (latestItem !== undefined) {
+          fetchMore();
+        }
+      }
+
+      prevReachedRef.current = hasReached;
+    };
+
+    // 最初は実行されないので手動で呼び出す
+    prevReachedRef.current = false;
+    handler();
+
+    document.addEventListener('wheel', handler, { passive: false });
+    document.addEventListener('touchmove', handler, { passive: false });
+    document.addEventListener('resize', handler, { passive: false });
+    document.addEventListener('scroll', handler, { passive: false });
+    return () => {
+      document.removeEventListener('wheel', handler);
+      document.removeEventListener('touchmove', handler);
+      document.removeEventListener('resize', handler);
+      document.removeEventListener('scroll', handler);
+    };
+  }, [latestItem, fetchMore]);
+
+  return <>{children}</>;
 };
 
 export { InfiniteScroll };
